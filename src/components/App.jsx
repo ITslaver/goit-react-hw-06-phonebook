129fd2c4042c0ecc78c5548197fc3fa61ec074ab
Viacheslav Polishchuk <mr_v.p@ukr.net>
2023-01-16 20:11:12 +0200

    bag fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,11 @@
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import { ContactForm } from "./ContactForm/ContactForm";
-import { Filter } from "./Filter/Filter";
-import { ContactList } from "./ContactList/ContactList";
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
 
 import css from './App.module.css';
-
 
 export const App = () => {
   return (
@@ -14,15 +13,11 @@ export const App = () => {
       <h1 className={css.phonebook__title}>Phonebook</h1>
       <ContactForm />
 
-      <h2 className={css.contacts__title}>Contacts</h2>
+      <h2 className={css.contacts__title}>Filter</h2>
       <Filter />
       <ContactList />
 
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        theme="colored"
-      />
+      <ToastContainer position="top-center" autoClose={3000} theme="colored" />
     </>
   );
 };
--- src/redux/filterSlice.js
+++ src/redux/filterSlice.js
@@ -1,19 +1,14 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
-// const state = {
-//   contacts: [],
-//   filter: "",
-// };
-  
 const filterSlice = createSlice({
-  name: "filters",
-  initialState: "",
+  name: 'filters',
+  initialState: '',
   reducers: {
     filterContacts(state, action) {
       return action.payload;
     },
   },
 });
-  
+
 export const { filterContacts } = filterSlice.actions;
 export const filtersReducer = filterSlice.reducer;
